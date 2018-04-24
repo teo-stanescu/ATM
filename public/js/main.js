1c3f6a0c67d9f19ae44a8972e1f49cdba26c94c8
teo-stanescu <38700990+teo-stanescu@users.noreply.github.com>
2018-04-25 00:46:48 +0300

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -2,20 +2,31 @@ var map;
 
 function initMap() {
 
-    map = getMap();
+    map = getMap(); //in GetMap.js; map customization
 
-    // var kmlLayer = new google.maps.KmlLayer();
-
-    var src = 'https://dl.dropboxusercontent.com/s/2nztszr17gnr4jm/LROP%20to%20EGLL.kml?dl=0';
-    var kmlLayer = new google.maps.KmlLayer(src, {
-            suppressInfoWindows: true,
-            preserveViewport: false,
-            map: map
-    });
-
-/*     var myParser = new geoXML3.parser({
+    var src = 'https://dl.dropboxusercontent.com/s/2nztszr17gnr4jm/LROP%20to%20EGLL.kml?dl=0'; //File uploaded to dropbox in order to de downloaded
+    var kmlLayer = new google.maps.KmlLayer(src, {  //define kmlLayer to add to map
+        suppressInfoWindows: true,
+        preserveViewport: false,
         map: map
     });
 
-    myParser.parse('E:\\Facultate\\Anul 4\\SEM II\\ATM\\programare\\test.kml'); */
+    google.maps.event.addListener(kmlLayer, "click", showInContentWindow);
+
+}
+
+
+function showInContentWindow(kmlEvent) {
+    var content = kmlEvent.featureData.infoWindowHtml; //retrive content of clicked element
+    var wpName = content.split("h3")[1].replace('<', '').replace('>', '').replace('/', '');   //get name from string
+    var infoWindow = new google.maps.InfoWindow({
+        content: wpName
+    });
+
+    infoWindow.setPosition(kmlEvent.latLng);
+    infoWindow.setOptions({
+        pixelOffset: kmlEvent.pixelOffset,
+        content: wpName
+    });
+    infoWindow.open(map);
 }
