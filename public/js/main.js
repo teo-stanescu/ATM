22bb5c910d12a9d804a1025f3b61f865eb056d75
teo-stanescu <38700990+teo-stanescu@users.noreply.github.com>
2018-04-26 06:59:23 +0300

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,7 @@ function initMap() {
     map = getMap(); //in GetMap.js; map customization
 
     var src = 'https://dl.dropboxusercontent.com/s/m59iii15yt4cr4u/route_adt2.kml?dl=0'; //File uploaded to dropbox in order to de downloaded
+    
     var kmlLayer = new google.maps.KmlLayer(src, {  //define kmlLayer to add to map
         suppressInfoWindows: true,
         preserveViewport: false,
@@ -17,9 +18,10 @@ function initMap() {
 
 function showInContentWindow(kmlEvent) {
     var content = kmlEvent.featureData.infoWindowHtml; //retrive content of clicked element
-    var wpName = content.split("h3")[1].replace('<', '').replace('>', '').replace('/', '');   //get name from string
+    var wpName = kmlEvent.featureData.name;   //get name from string
+    var wpDescription = parseDescription(kmlEvent.featureData.description);
     var infoWindow = new google.maps.InfoWindow({
-        content: wpName
+        content: wpDescription.speed + '<br>' + wpDescription.heading 
     });
 
     infoWindow.setPosition(kmlEvent.latLng);
@@ -28,4 +30,16 @@ function showInContentWindow(kmlEvent) {
         content: wpName
     });
     infoWindow.open(map);
+}
+
+function parseDescription(description)
+{
+    var splitUp = description.split(" ");
+    var data = {
+        speed: parseInt(splitUp[0]),
+        heading: parseFloat(splitUp[1]),
+        timestamp: "+" + splitUp[2]
+    }
+
+    return data;
 }
