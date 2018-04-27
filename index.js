2e8e7019e5bce5c00502d5989016c813208aba06
teo-stanescu <38700990+teo-stanescu@users.noreply.github.com>
2018-04-27 13:35:40 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function (req, res, next) {
+/* app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
+}); */
 
 app.get('/', function (req, res, next) {
     res.sendFile(path.join(__dirname + '/index.html'));
--- public/js/main.js
+++ public/js/main.js
@@ -2,20 +2,76 @@ var map;
 
 function initMap() {
 
-    map = getMap();
+    map = getMap(); //in GetMap.js; map customization
 
-    // var kmlLayer = new google.maps.KmlLayer();
-
-    var src = 'https://dl.dropboxusercontent.com/s/2nztszr17gnr4jm/LROP%20to%20EGLL.kml?dl=0';
-    var kmlLayer = new google.maps.KmlLayer(src, {
-            suppressInfoWindows: true,
-            preserveViewport: false,
-            map: map
-    });
-
-/*     var myParser = new geoXML3.parser({
+    var src = 'https://dl.dropboxusercontent.com/s/m59iii15yt4cr4u/route_adt2.kml?dl=0'; //File uploaded to dropbox in order to de downloaded
+    var kmlLayer = new google.maps.KmlLayer(src, {  //define kmlLayer to add to map
+        suppressInfoWindows: true,
+        preserveViewport: false,
         map: map
     });
+    plotVeticalProfile (kmlLayer);
+    google.maps.event.addListener(kmlLayer, "click", showInContentWindow);
+}
 
-    myParser.parse('E:\\Facultate\\Anul 4\\SEM II\\ATM\\programare\\test.kml'); */
+function plotVeticalProfile (kmlEvent) {
+    var ctx = document.getElementById("verticalProfile");  
+    var time = new Array;
+    var flightLevel = new Array;
+    myData.features.forEach(function (element) {
+        if (element.properties.Description) time.push(element.properties.Description.split(" ")[2].replace(".",":").replace(".",":"));
+        if (element.properties.Description) flightLevel.push(element.properties.Description.split(" ")[3]);
+    });
+    var chart =  new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: time,
+            datasets: [{
+                label: 'Flight Level',
+                data: flightLevel,
+                backgroundColor: '#E6AC60',
+                borderWidth: 1,
+                fill: true
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+}
+
+function showInContentWindow(kmlEvent) {
+    
+    var wpName = kmlEvent.featureData.name;   //get name from string
+    var wpDescription = parseDescription(kmlEvent.featureData.description);
+    var infoWindow = new google.maps.InfoWindow({
+        
+    });
+
+    infoWindow.setPosition(kmlEvent.latLng);
+    infoWindow.setOptions({
+        pixelOffset: kmlEvent.pixelOffset,
+        content: wpName + "<br>Speed: " + wpDescription.speed + "<br>Heading: " + wpDescription.heading + "<br>TimeStamp: " + wpDescription.timeStamp
+    });
+    infoWindow.open(map);
+
+    plotVeticalProfile(kmlEvent);
+}
+
+function parseDescription(description)
+{
+    var splitUp = description.split(" ");
+    var data = {
+        speed: parseInt(splitUp[0]),
+        heading: parseFloat(splitUp[1]),
+        timeStamp: "+" + splitUp[2].replace(".",":")
+    }
+
+    return data;
 }
